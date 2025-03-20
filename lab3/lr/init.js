2ec528734ee8803ea317ac8edc999774beef94d2
ser <ashkae1@ya.ru>
2025-03-20 09:11:21 +0300

    Удалены

--- lab3/lr/init.js
+++
@@ -1,9 +0,0 @@
-import {MainPage} from "./pages/main/index.js";
-
-export function initLR() {
-    const root = document.getElementById('lr-root');
-    if (root) {
-        const mainPage = new MainPage(root);
-        mainPage.render();
-    }
-}
